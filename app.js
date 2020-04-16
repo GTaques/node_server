4d7b66363ba207b4968e03d62eb7d931bff53197
Gabriel Taques <gabrielpctaques@gmail.com>
2020-04-16 12:21:24 -0300

    added connection

--- app.js
+++ app.js
@@ -8,26 +8,26 @@ var CONTACTS_COLLECTION = "contacts";
 var app = express();
 app.use(bodyParser.json());
 
-// // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-// var db;
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
 
-// // Connect to the database before starting the application server.
-// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-//   if (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-//   // Save database object from the callback for reuse.
-//   db = client.db();
-//   console.log("Database connection ready");
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
 
-//   // Initialize the app.
-//   var server = app.listen(process.env.PORT || 8000, function () {
-//     var port = server.address().port;
-//     console.log("App now running on port", port);
-//   });
-// });
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8000, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
 
 app.use(express.json())
 
