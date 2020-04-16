9efd72758d0a16bfa5a934fe6323009d8ffa05b9
Gabriel Taques <gabrielpctaques@gmail.com>
2020-04-16 12:19:44 -0300

    changed port

--- app.js
+++ app.js
@@ -8,26 +8,26 @@ var CONTACTS_COLLECTION = "contacts";
 var app = express();
 app.use(bodyParser.json());
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
+// // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+// var db;
 
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+// // Connect to the database before starting the application server.
+// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
 
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
+//   // Save database object from the callback for reuse.
+//   db = client.db();
+//   console.log("Database connection ready");
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8000, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
+//   // Initialize the app.
+//   var server = app.listen(process.env.PORT || 8000, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+//   });
+// });
 
 app.use(express.json())
 
@@ -35,9 +35,9 @@ app.get('/', function(req, res) {
     res.send("Seja bem vindo ")
 })
 
-app.get('/todos', function(req, res) {
+// app.get('/todos', function(req, res) {
 
-})
+// })
 
 let port = process.env.PORT;
 if (port == null || port == "") {
@@ -48,8 +48,8 @@ app.listen(port, function() {
 });
 
 
-async function createToDo(client, newToDo) {
-  const result = await client.db('done_it').collection('todos').insertOne(newToDo);
-  console.log(`${result.insertedCount} new todo(s) created with the following id(s):`);
-  console.log(result.insertedIds);
-}
+// async function createToDo(client, newToDo) {
+//   const result = await client.db('done_it').collection('todos').insertOne(newToDo);
+//   console.log(`${result.insertedCount} new todo(s) created with the following id(s):`);
+//   console.log(result.insertedIds);
+// }
