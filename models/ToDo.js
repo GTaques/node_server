140d8bc08b2ab942a2649cc6c01d9505058ef106
Gabriel Taques <gabrielpctaques@gmail.com>
2020-04-17 14:45:45 -0300

    Added new attributes to model

--- models/ToDo.js
+++ models/ToDo.js
@@ -5,9 +5,25 @@ const ToDoSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    notes: {
+        type:  String,
+        required: false
+    },
+    remindMeOnDay: {
+        type: Boolean,
+        default: false
+    },
+    remindDate: {
+        type: Date,
+        required: false
+    },
     priority: {
         type: Number,
         required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
     },
     createdAt: {
         type: Date,
